2d46fac21a17065f5e560f5acf133cc614f071c6
Raphael Serafim <raphaelvserafim@gmail.com>
2024-07-31 20:46:26 -0400

    ♻️ refactor: project

---
+++ src/app/Navigation.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { RootStackParamList } from '@/model';
+import { HomeScreen } from '@/screens';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function Navigation() {
+  return (
+    <React.Fragment>
+      <StatusBar barStyle="light-content" backgroundColor="#002c60" animated={true} />
+      <NavigationContainer >
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </React.Fragment>
+  );
+}
+
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import style from "@/styles/Home";
+
+export function HomeScreen() {
+  return (
+    <View style={style.container}>
+      <Text>Hello Word</Text>
+    </View>
+  );
+}
