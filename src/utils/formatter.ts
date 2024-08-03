ca0feda61a62afc50b95c7e2d6d7135f86f74a93
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-02 20:32:48 -0400

    ✨ feat: formatter  categories and posts

---
+++ src/utils/formatter.ts
@@ -0,0 +1,41 @@
+import { cleanCategoryName, truncateText } from "@/utils";
+
+export function formatCategories(categories: any[]): { id: number; name: string; slug: string; description: string; count: number }[] {
+  return categories.map((category: {
+    id: number;
+    name: string;
+    slug: string;
+    description: string;
+    count: number;
+  }) => ({
+    id: category.id,
+    name: cleanCategoryName(category?.name),
+    slug: category.slug,
+    description: category.description,
+    count: category.count,
+  }));
+}
+
+export async function formatPosts(posts: any[], fetchFeaturedImageUrl: (imageId: number) => Promise<string>): Promise<{ id: number; title: string; date: string; excerpt: string; link: string; imageUrl: string }[]> {
+  const formattedPosts = posts.map(async (post: {
+    id: number;
+    title: { rendered: string };
+    date: string;
+    excerpt: { rendered: string };
+    link: string;
+    featured_media?: number;
+  }) => {
+    const featuredImageId = post.featured_media;
+    const featuredImage = featuredImageId ? await fetchFeaturedImageUrl(featuredImageId) : '';
+    return {
+      id: post.id,
+      title: truncateText(post.title.rendered, 60),
+      date: post.date,
+      excerpt: post.excerpt.rendered,
+      link: post.link,
+      imageUrl: featuredImage,
+    };
+  });
+
+  return Promise.all(formattedPosts);
+}
