7a3966924f254b2d8d59830a562ca69e8956de70
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-03 23:46:27 -0400

    💄 feat: formatted

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,6 +1,13 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
 export type RootStackParamList = {
   Home: undefined;
+  NewsDetail: { id: number };
 };
+
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
 
 export interface NewsCardProps {
   title: string;
@@ -10,14 +17,56 @@ export interface NewsCardProps {
 }
 
 export interface ListRenderItemInfo {
-  id: string;
+  id: number;
   title: string;
   description: string;
   imageUrl: string;
   link: string;
 }
 
+
+export type Post = {
+  id: number;
+  date: string;
+  title: { rendered: string };
+  excerpt: { rendered: string };
+  content: { rendered: string };
+  link: string;
+  author: number;
+  _embedded?: {
+    'wp:featuredmedia'?: [
+      {
+        media_details?: {
+          sizes?: {
+            thumbnail?: { source_url: string };
+            full?: { source_url: string };
+          };
+        };
+        source_url: string;
+      }
+    ];
+  };
+};
+
+export type FormattedPost = {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+  link: string;
+  thumbnailUrl: string;
+  fullImageUrl: string;
+  content: string;
+};
+
+
 export interface NewsCardAProps {
-  item: ListRenderItemInfo;
-  onPress: (item: ListRenderItemInfo) => void;
+  item: FormattedPost;
+  onPress: (item: FormattedPost) => void;
+}
+
+
+export interface PostsContextType {
+  posts: Post[];
+  addPosts: (newPosts: Post[]) => void;
 }
